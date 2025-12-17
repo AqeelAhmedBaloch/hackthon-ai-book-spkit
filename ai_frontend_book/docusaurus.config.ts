7f57e8cc1c92821c6e10dd19525eedae13f927ea
Aqeel Ahmed Baloch <ahmedbalochaqeel@gmail.com>
2025-12-17 20:18:20 +0500

    Update

--- ai_frontend_book/docusaurus.config.ts
+++ ai_frontend_book/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-organization.github.io',
+  url: 'https://ai-humanoid-robotics.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ai-humanoid-robotics/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
