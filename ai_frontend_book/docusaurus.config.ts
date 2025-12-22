08b98c904862f0a0376bd77a17ee95bb845956a3
Aqeel Ahmed Baloch <151179923+AqeelAhmedBaloch@users.noreply.github.com>
2025-12-22 10:31:26 +0500

    Update

--- ai_frontend_book/docusaurus.config.ts
+++ ai_frontend_book/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://ai-humanoid-robotics.vercel.app',
+  url: 'https://hackthon-ai-book-spkit.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
